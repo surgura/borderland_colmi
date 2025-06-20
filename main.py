023aa53ea5ec0f436702ce5a2697fe34a09c1953
Aart Stuurman <aartstuurman@hotmail.com>
2025-06-20 16:11:11 +0200

    easier comds

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from colmi_r02_client.client import Client
 from bleak import BleakScanner, BLEDevice
 import asyncio
 from colmi_r02_client.cli import DEVICE_NAME_PREFIXES
+import argparse
 
 def find_devices() -> list[BLEDevice]:
     all_devices = asyncio.run(BleakScanner.discover(timeout=10))
     rings = [d for d in all_devices if d.name and any([d.name.startswith(p) for p in DEVICE_NAME_PREFIXES])]
     return rings
 
-def print_devices() -> None:
+def cmd_scan() -> None:
+    print("Scanning..")
     ring_devices = find_devices()
     if len(ring_devices) == 0:
         print("No rings found.")
@@ -19,9 +21,37 @@ def print_devices() -> None:
         for ring in ring_devices:
             print(f"{ring.name:>20}  |  {ring.address}")
 
+def cmd_run(addresses: list[str]) -> None:
+    print(f"Running with addresses:\n{'\n'.join(addresses)}")
+    raise NotImplementedError()
+
+def cmd_scan_and_run() -> None:
+    print("Scanning..")
+    ring_devices = find_devices()
+    if len(ring_devices) == 0:
+        print("No rings found. Exiting..")
+        return
+    cmd_run([ring.address for ring in ring_devices])
+
 def main() -> None:
-    # Client()
-    print_devices()
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command", required=True)
+
+    scan_parser = subparsers.add_parser("scan")
+
+    run_parser = subparsers.add_parser("run")
+    run_parser.add_argument("addresses", nargs="+", help="Addresses to run with")
+
+    scan_and_run_parser = subparsers.add_parser("scan_and_run")
+
+    args = parser.parse_args()
+
+    if args.command == "scan":
+        cmd_scan()
+    elif args.command == "run":
+        cmd_run(args.addresses)
+    elif args.command == "scan_and_run":
+        cmd_scan_and_run()
 
 
 if __name__ == "__main__":
